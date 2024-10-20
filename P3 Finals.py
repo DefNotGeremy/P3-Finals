9bfbaaae4e46855759b3c517e0a95e408645d95e
Geremy Roi Vila <geremyroivila@gmail.com>
2024-10-20 18:44:27 +0800

    Update P3 Finals.py
    Changed so when you choose wrong option, you can choose again. Fixed routes.

--- P3 Finals.py
+++ P3 Finals.py
@@ -1,5 +1,6 @@
+#Start of the story
 def main():
-    print("Dialogue here")
+    print("Start of story")
     print("Select option:")
     print("1. Option 1")
     print("2. Option 2")
@@ -15,88 +16,124 @@ def main():
                 
     if choice == '1':
         print(story1, continue1)
+        continuation()
 
     elif choice == '2':
         print(story2, continue1)
+        continuation()
 
     elif choice == '3':
         print(story3, continue1)
+        continuation()
 
     elif choice == '4':
         print("User has left")
         exit()
+    
+    else:
+        print("Invalid choice. Please try again.")
+        main()
 
+def continuation():
     print("Select option:")
-    print("1. Option 1")
-    print("2. Option 1")
-    print("3. Option 1")
+    print("1. Option 1") 
+    print("2. Option 2")
+    print("3. Option 3")
     print("4. End story")
-
-    story4 = "Dialogue 4"
-    story5 = "Dialogue 5"
-    story6 = "Dialogue 6"
-    continue2 = "Continue 2"
-    continue3 = "Continue 3"
 
     choice = input("Enter choice (1/2/3/4): ")
             
     if choice == '1':
+        story4 = "Dialogue 4"
+        continue2 = "Continue 2"
         print(story4, continue2)
+        friendship()
 
     elif choice == '2':
-        print(story5, continue2)
-        flirtt()
+        story5 = "Story 5"
+        continue3 = "Continue 3"
+        print(story5, continue3)
+        flirt()
 
     elif choice == '3':
-        print(story6, continue3)
+        story6 = "Dialogue 6"
+        continue4 = "Continue 4'"
+        print(story6, continue4)
         lie()
 
     elif choice == '4':
         print("User has left")
         exit()
+    
+    else:
+        print("Invalid choice. Please try again.")
+        continuation()
 
-def flirt():
-    story7 = "Dialogue 7"
-    continue4 = "Continue 4"
-    print(story7, continue4)
-    flirt_options()
-
-def flirt_options():
+#friendship route
+def friendship():
     print("Select option:")
-    print("1. ")
-    print("2. ")
-    print("3. ")
+    print("1. Option 1")
+    print("2. Option 2")
+    print("3. End story")
+
+    choice = input("Enter choice (1/2/3): ")
+    if choice == '1':
+        print("")
+    
+    elif choice == '2':
+        print("")
+    
+    elif choice == '3':
+        print("User has left")
+        exit()
+
+    else:
+        print("Invalid choice. Please try again.")
+        friendship()
+
+#romance route
+def flirt():
+    print("Select option:")
+    print("1. Option 1")
+    print("2. Option 2")
+    print("3. End story")
     
     choice = input("Enter choice (1/2/3): ")
     if choice == '1':
-        print("Text here")
+        print("")
+    
     elif choice == '2':
-        print("Text here")
+        print("")
+    
     elif choice == '3':
-        print("Text here")
+        print("User has left")
         exit()
+    
+    else:
+        print("Invalid choice. Please try again.")
+        flirt()
 
+#creepy route
 def lie():
-    story8 = "Dialogue 8"
-    continue5 = "Continue 5"
-    print(story8, continue5)
-    lie_options()
-
-def lie_options():
     print("Select option:")
-    print("1. ")
-    print("2. ")
-    print("3. ")
+    print("1. Option 1")
+    print("2. Option 2")
+    print("3. End story")
 
     choice = input("Enter choice (1/2/3): ")
     if choice == '1':
-        print("Text here")
+        print("")
+    
     elif choice == '2':
-        print("Text here")
+        print("")
+    
     elif choice == '3':
-        print("Text here")
+        print("User has left")
         exit()
-
+    
+    else:
+        print("Invalid choice. Please try again.")
+        lie()
 
 if __name__ == "__main__":
     main()
